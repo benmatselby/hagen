3a878ac301507c99bd1fe860ed5bf129a7660128
Ben Selby <benmatselby@gmail.com>
2020-05-02 18:47:50 +0100

    Allow recursion in the `issues` command

--- cmd/list_issues.go
+++ cmd/list_issues.go
@@ -13,10 +13,12 @@ import (
 
 // ListIssuesOptions defines what arguments/options the user can provide
 type ListIssuesOptions struct {
-	Args     []string
-	Count    int
-	Query    string
-	Template string
+	Args      []string
+	Count     int
+	Query     string
+	Recursive bool
+	Template  string
+	Verbose   bool
 }
 
 // NewListIssuesCommand will register the `issues` command
@@ -28,21 +30,46 @@ func NewListIssuesCommand(client hagen.Provider) *cobra.Command {
 		Short: "List issues given the search criteria. Default query is to list issues where the author is ${GITHUB_OWNER}",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts.Args = args
-			query, searchOpts := NewSearchFromIssueOptions(opts)
 
-			result, err := client.ListIssues(query, searchOpts)
-			if err != nil {
-				return err
+			more := true
+			query := ""
+			received := 0
+			for page := 1; more; page++ {
+				query, searchOpts := NewSearchFromIssueOptions(opts)
+				searchOpts.Page = page
+				result, err := client.ListIssues(query, searchOpts)
+				if err != nil {
+					return err
+				}
+
+				err = DisplayIssues(result, os.Stdout)
+				if err != nil {
+					return err
+				}
+
+				received += len(result.Issues)
+				more = received < result.GetTotal()
+
+				if !opts.Recursive {
+					fmt.Printf("\nPress enter for more results\n")
+					fmt.Scanln()
+				}
 			}
 
-			return DisplayIssues(result, os.Stdout)
+			if opts.Verbose {
+				fmt.Fprintf(os.Stdout, "\n\nQuery used: %s\n", query)
+			}
+
+			return nil
 		},
 	}
 
 	flags := cmd.Flags()
 	flags.IntVar(&opts.Count, "count", -1, "How many issues to display")
 	flags.StringVar(&opts.Query, "query", "", "The search query to get issues")
+	flags.BoolVar(&opts.Recursive, "recursive", false, "Do you want to recursively get all results")
 	flags.StringVar(&opts.Template, "template", "", "Use a query defined in the configuration file")
+	flags.BoolVar(&opts.Verbose, "verbose", false, "Produce verbose output")
 
 	return cmd
 }
@@ -75,7 +102,7 @@ func NewSearchFromIssueOptions(opts ListIssuesOptions) (string, github.SearchOpt
 // DisplayIssues will display issues based on the given search criteria
 func DisplayIssues(result *github.IssuesSearchResult, w io.Writer) error {
 	for _, issue := range result.Issues {
-		fmt.Fprintf(w, "* #%v %s\n", issue.GetNumber(), issue.GetTitle())
+		fmt.Fprintf(w, "- #%v %s\n", issue.GetNumber(), issue.GetTitle())
 	}
 	return nil
 }
