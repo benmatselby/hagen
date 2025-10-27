23282c87ce67442a6a57f12243a987253e8098d2
Ben Selby <ben.selby@prolific.com>
2025-10-27 19:12:27 +0000

    Refactor the date formats into constants

--- ui/ui.go
+++ ui/ui.go
@@ -1,4 +1,12 @@
+// Package ui contains user interface related configuration
 package ui
 
 // MoreResults provides a consistent message to get more results
 const MoreResults = "\nPress enter for more results\n"
+
+// HumanFriendlyDateFormat is a date format string for displaying dates in a
+// human friendly way
+const HumanFriendlyDateFormat = "Monday 02 January, 2006 at 15:04 MST"
+
+// DateFormat is the standard date format string
+const DateFormat = "2006-01-02 15:04:05 -07:00"
